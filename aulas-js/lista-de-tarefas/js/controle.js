d9d0aae541a2766a5df8a3402636faa580e30ab4
reuelsilva <reuelsilva19@gmail.com>
2023-01-27 20:20:09 -0300

    novo projeto lista de tarefas

---
+++ aulas-js/lista-de-tarefas/js/controle.js
@@ -0,0 +1,63 @@
+let input = document.getElementById('inputTarefa');
+let btnAdd = document.getElementById('btn-Add');
+let main = document.getElementById('areaLista');
+let contador = 0;
+
+function addTarefa(){
+    let valorInput = input.value; /*PEGOU O VALOR DO ELEMENTO INPUT*/
+    if((valorInput != '') && (valorInput != null) && (valorInput != undefined)){
+        
+        contador += 1;
+
+        let novoItem = `<div id="${contador}" class="item">
+        <div class="item-icone">
+            <i id="icone_${contador}" onclick="marcarTarefa(${contador})" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-tarefa">
+            ${valorInput}
+        </div>
+        <div class="item-delete">
+            <button onclick="deletar(${contador})" class="delete"> <i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+    </div>`
+
+    main.innerHTML += novoItem;
+    input.value = '';
+    console.log('teste')
+    input.focus();
+    }
+}
+
+function marcarTarefa(id){
+    let item = document.getElementById(id);
+    let classe = item.getAttribute('class');
+    if(classe == "item"){
+        item.classList.add('clicado');
+
+        let icone = document.getElementById(`icone_${id}`);
+        icone.classList.remove('mdi-circle-outline');//REMOVE CLASSE
+        icone.classList.add('mdi-check-circle');//ADICIONA CLASSE
+
+        item.parentNode.appendChild(item);
+    }else{
+        item.classList.remove('clicado');
+        
+        let icone = document.getElementById(`icone_${id}`);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    } 
+}
+
+function deletar(id){
+    let item = document.getElementById(id);
+    item.remove();
+}
+
+//INPUT RESPONDER AO COMANDO DE PRESSIONAMENTO DE TECLA
+input.addEventListener('keyup', function(event){
+    if(event.keyCode == 13){
+        btnAdd.click();
+    }
+})
+
+
